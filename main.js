2b89ade053d4e66708e67c6ba5aec68a831f21bf
Warcharoth <34127177+AciDBurneR0@users.noreply.github.com>
2022-03-18 15:47:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -60,12 +60,6 @@ client.on("messageCreate", (message) => {
     botResponse.send(
       `Tu dovresti essere quello stronzo rotto n'culo de ${message.author.username}`
     );
-
-    botResponse.send(
-      `${message.author.iconURL}`
-    );
-
-    console.log(`${message.author.iconURL}`);
     /* setTimeout(() => {
       botResponse.send('E non me lo chiedere mai più');
     }, 5000); */
