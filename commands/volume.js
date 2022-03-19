c8bbcbe6f22de8b1980ded221e85d8a256fc8aa2
Warcharoth <34127177+AciDBurneR0@users.noreply.github.com>
2022-03-19 23:26:49 +0100

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -6,7 +6,9 @@ module.exports = {
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | Che t'abbasso, sto cazzo t'abbasso!`)
     const volume = parseInt(args[0])
+    console.log(volume > 100, 'VOLUME');
     if (isNaN(volume)) return message.channel.send(`${client.emotes.error} | Ma li mortacci tua damme un valore normale!`)
+    if (volume > 100) return message.channel.send(`${client.emotes.error} | Ciccia ar culo`)
     queue.setVolume(volume)
     message.channel.send(`${client.emotes.success} | Casse settate al \`${volume}\`%`)
   }
